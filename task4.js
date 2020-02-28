5a548c1913b03ded432e21ebfddab03f3497cb1f
Kenyatta Hill <khill@insight2profit.com>
2020-02-28 16:07:43 -0500

    solution for task-4

---
+++ task4.js
@@ -0,0 +1,8 @@
+const sentence = ' Hey, this is Task 4 of JS Path! ';
+console.log(sentence.length);
+console.log(sentence.indexOf('Task'))
+console.log(sentence.slice(24, 26));
+const replace = sentence.replace('JS', 'Python');
+console.log(replace);
+console.log(replace.concat('!'))
+console.log(replace.concat('!').trim())

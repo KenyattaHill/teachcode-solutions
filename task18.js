12d4d39910e16387bccacf2aab7c4d2ca2654e68
Kenyatta Hill <khill@insight2profit.com>
2020-03-03 13:34:18 -0500

    solution for task-18

---
+++ task18.js
@@ -0,0 +1,8 @@
+const a = true.toString();
+const b = false.toString();
+console.log(typeof a);
+console.log(typeof b);
+const c = true;
+const d = false;
+console.log(typeof c);
+console.log(typeof d);

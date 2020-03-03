93726c9d7a688c000ccc11b90dfdb3d0a2c6598b
Kenyatta Hill <khill@insight2profit.com>
2020-03-03 13:43:56 -0500

    solution for task-19

--- task17.js
+++ task17.js
@@ -1,10 +1,12 @@
-const days = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday'
-];
-console.log(days[new Date().getDay()]);
+// const days = [
+//   'Sunday',
+//   'Monday',
+//   'Tuesday',
+//   'Wednesday',
+//   'Thursday',
+//   'Friday',
+//   'Saturday'
+// ];
+// console.log(days[new Date().getDay()]);
+
+console.log(new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(new Date()));

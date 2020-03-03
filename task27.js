e0e56b90ac606304db1a45f6a2a75a99d5b7490e
Kenyatta Hill <khill@insight2profit.com>
2020-03-03 14:27:17 -0500

    solution for task-27

--- task27.js
+++ task27.js
@@ -1,9 +1,13 @@
 const test = ['abc', 'bcd', 'cde'];
+
 test.forEach(x => console.log(x));
-for (const key in test) {
-  console.log(key)
-}
 
 for (const value of test) {
-  console.log(value)
+  console.log(value);
 }
+
+for (const key in test) {
+  console.log(key);
+}
+
+
